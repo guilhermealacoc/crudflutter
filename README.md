94856fa49eb0de81517c6ee7d8a2568dac5cc14f
Guilherme Alacoc <guilhermealacoc@gmail.com>
2020-07-01 18:57:44 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# crudflutter
+ A CRUD application with Flutter and SQLite
---
+++ android/app/src/main/kotlin/com/alacoc/crudflutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.alacoc.crudflutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
